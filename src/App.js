5c37cccd4d894f21e6df567f373875956e862aa7
SecretAgent <iyengar.dhruv@outlook.com>
2023-10-09 18:38:29 -0700

    hash router might work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import CounselorsPage from './components/CounselorsPage';
