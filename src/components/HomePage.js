42b1e84e8e048fa2fb02b57964e61fe7bd71a086
FishoGithub <68925102+FishoGithub@users.noreply.github.com>
2023-10-14 11:08:55 -0700

    BUILD COMPILED SUCCESSFULY LETS GO

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -17,45 +17,88 @@ const HomePage = () => {
   // })
 
   return (
-    <><><><div id='title'>
-      <h1>Redmond STEM</h1>
-    </div><div id='intro_paragraphs'>
+    <div>
+      <head>
+        <link rel='stylesheet' href="https://fonts.google.com/specimen/Gabarito"/>
+        <link rel='stylesheet' type = 'text/css' href='HomePage.css'></link>
+      </head>
+      <div style ={{
+          
+      }}>
+        <h1>Redmond STEM</h1>
+      </div>
+      <div id="intro_paragraphs">
         {/* first two paragraphs which intro what the org is about */}
-
-        <p>We are an organization of students comitted to teaching kids of all ages, (primarily grades 5-8)
+        <p>We are an organization of students comitted to teaching kids of all ages, (primarily grades 5-8) 
           the fundamentals of science, technology, engineering, and mathematics. Over 4-8 weeks,
           students gain a beginner to intermediate level of knowledge on each topic.</p>
-
         <p>At Redmond STEM, we make learning fun for all ages, while still provinding a good introduction to our students on
           a topic, so they remain interested for years to come, and can even carry their knowledge into a higher education.
           Focusing primarily on grades 5-8, learning should be fun and interesting for the student, and our curriculum does
-          just that, ensuring the child continues to enjoy the subject even after the course has ended.
+          just that, ensuring the child continues to enjoy the subject even after the course has ended. 
         </p>
-      </div></><div id='inspirational_quote'>
-        <h4>
-          "The roots of education are bitter, but the fruit is sweet."
-        </h4>
-        <h4 id="aristotle">
-          -Aristotle
-        </h4>
-      </div></><div id="courses_offered">
-
+      </div>
+      <div id="inspirational_quote" >
+      <h3>
+        "The roots of education are bitter, but the fruit is sweet." <h3 id="aristotle">-Aristotle</h3>
+      </h3>
+      </div>
+      <div id="courses_offered">
         <h4 id="courses_title">
-          Courses Offered
+          Learning for every age group & subject
         </h4>
+        <div id='list_of_courses'>
+          <ul>
+            <li>INTRO TO ALGEBRA</li>
+            <li>INTRO TO GEOMETRY</li>
+            <li>INTRO TO COMPUTER SCIENCE</li>
+            <li>AMC8 SUPPLEMENT</li>
+            <li>SCIENCE OLYMPIAD SUPPPLEMENT</li>
+            <li>INTRO TO ALGEBRA 2</li>
+          </ul>
+        </div>
 
-        <ul>
-          <li>INTRO TO ALGEBRA</li>
-          <li>INTRO TO GEOMETRY</li>
-          <li>INTRO TO COMPUTER SCIENCE</li>
-          <li>AMC8 SUPPLEMENT</li>
-          <li>SCIENCE OLYMPIAD SUPPPLEMENT</li>
-          <li>INTRO TO BIOLOGY</li>
-          <li>INTRO TO ALGEBRA 2</li>
-        </ul>
-      </div></>
-    
-      
+      </div>
+    </div>
+
   )
 }
 export default HomePage;
+
+// <><><><div id='title'>
+    //   <h1>Redmond STEM</h1>
+    // </div><div id='intro_  paragraphs'>
+    //     {/* first two paragraphs which intro what the org is about */}
+
+    //     <p>We are an organization of students comitted to teaching kids of all ages, (primarily grades 5-8)
+    //       the fundamentals of science, technology, engineering, and mathematics. Over 4-8 weeks,
+    //       students gain a beginner to intermediate level of knowledge on each topic.</p>
+
+    //     <p>At Redmond STEM, we make learning fun for all ages, while still provinding a good introduction to our students on
+    //       a topic, so they remain interested for years to come, and can even carry their knowledge into a higher education.
+    //       Focusing primarily on grades 5-8, learning should be fun and interesting for the student, and our curriculum does
+    //       just that, ensuring the child continues to enjoy the subject even after the course has ended.
+    //     </p>
+    //   </div></><div id='inspirational_quote'>
+    //     <h4>
+    //       "The roots of education are bitter, but the fruit is sweet."
+    //     </h4>
+    //     <h4 id="aristotle">
+    //       -Aristotle
+    //     </h4>
+    //   </div></><div id="courses_offered">
+
+    //     <h4 id="courses_title">
+    //       Courses Offered
+    //     </h4>
+
+    //     <ul>
+    //       <li>INTRO TO ALGEBRA</li>
+    //       <li>INTRO TO GEOMETRY</li>
+    //       <li>INTRO TO COMPUTER SCIENCE</li>
+    //       <li>AMC8 SUPPLEMENT</li>
+    //       <li>SCIENCE OLYMPIAD SUPPPLEMENT</li>
+    //       <li>INTRO TO BIOLOGY</li>
+    //       <li>INTRO TO ALGEBRA 2</li>
+    //     </ul>
+    //   </div></>
