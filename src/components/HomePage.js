b39daa16990e69f76c90790cc878ea431a724e80
FishoGithub <68925102+FishoGithub@users.noreply.github.com>
2023-10-14 11:28:21 -0700

    created new components for homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,38 @@
+import Header from "./Header";
+import IntroParagraphs from "./IntroParagraphs";
+
 const HomePage = () => {
 
   return (
     <div>
+<<<<<<< Updated upstream
       <h1>Home Page</h1>
       <p>This is the home page!</p>
+=======
+      <Header/>
+      <IntroParagraphs/>
+      <div id="inspirational_quote" >
+      <h3>
+        "The roots of education are bitter, but the fruit is sweet." <h3 id="aristotle">-Aristotle</h3>
+      </h3>
+      </div>
+      <div id="courses_offered">
+        <h4 id="courses_title">
+          Learning for every age group & subject
+        </h4>
+        <div id='list_of_courses'>
+          <ul>
+            <li>INTRO TO ALGEBRA</li>
+            <li>INTRO TO GEOMETRY</li>
+            <li>INTRO TO COMPUTER SCIENCE</li>
+            <li>AMC8 SUPPLEMENT</li>
+            <li>SCIENCE OLYMPIAD SUPPPLEMENT</li>
+            <li>INTRO TO ALGEBRA 2</li>
+          </ul>
+        </div>
+
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 
